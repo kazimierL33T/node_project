04fe89853a4d144b7f173659c9f999d5ab92f254
Kazimier Dooley <kazimierdooley1999@gmail.com>
2026-03-26 10:22:57 -0600

    update

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
+const ejs = require('ejs');
 
 //importing of userRoutes route handler
 const userRoutes = require('./routes/userRoutes');
 
-//route handler
-app.use(userRoutes);
 
+//view engine setup
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+//middleware and route handlers
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(userRoutes);
 
 //server setup
 app.listen(port, () => {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+//home page
+router.get('/home', (req, res) => {
+    res.render('home');
+})
 
 
-//route handler
+    //example of a middleware functioon
+    router.use((req, res, next) => {
+        console.log(`Request URL: ${req.url}. Time: ${new Date()}`);
+    next();
+});
+
+//route handler //route parameters
 router.get('/', (req, res) => {
     res.send('Hello World! <3');
 });
@@ -19,6 +29,13 @@ router.get('/scary', (req, res) =>{
 router.get('/ghosts/:id', (req, res) => {
     const { id } = req.params;
     res.send(`This is the ghost page for ghost ${req.params.id}! :O`);
+});
+
+//query parameters
+router.get('/search', (req, res) => {
+    res.send(`search query: ${req.query.q}`);
 }); 
+
+
 
 module.exports = router;
