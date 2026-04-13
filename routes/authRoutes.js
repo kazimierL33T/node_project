65f2079a425889bdd753f5a2d5f48bec9ef15a19
Kazimier Dooley <kazimierdooley1999@gmail.com>
2026-04-13 11:24:40 -0600

    postman testing

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authControllers.js');
 
+
+
 router.get('/generate-token', authController.generateToken);
 
-
+router.post('/verify-token', authController.verifyToken);
 
 
 
