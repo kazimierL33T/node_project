ef4578615b690dcec3449a8c2269d623c3e9bffa
Kazimier Dooley <kazimierdooley1999@gmail.com>
2026-04-13 11:11:56 -0600

    status response updates

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -16,4 +16,28 @@ res.json({ token });
 
 }
 
-module.exports = { generateToken }
+async function verifyToken(req, res){
+    const secret_key = 'super_secret_key';
+
+    //extract token froom request header
+    //some_text auth_token
+    const token = req.headers['authorization']?.split(' ')[1];
+
+    if(!token) res.status(401).json({ message: 'You are not authorized.' });
+    
+    
+    try {
+        const decoded = jwt.verify(token, secret_key);
+
+        if(decoded){
+            res.json({ message: 'Token is valid.', decoded });
+        }else{
+            res.status(401).json({ message: 'Invalid token.' });
+        }
+
+    }catch(error){
+        res.status(500).json({ message: 'Internal server error.' });
+    }
+}
+
+module.exports = { generateToken, verifyToken }
